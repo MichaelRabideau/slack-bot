20f3db00495a46d11ee60eb1e905a044e8f22361
David <davidharrigan@users.noreply.github.com>
2021-04-03 14:57:09 -0400

    fix health url

--- bot/actions/healthcheck.py
+++ bot/actions/healthcheck.py
@@ -8,7 +8,7 @@ from bot.scheduler import add_schedule
 @add_schedule(name='healthcheck', schedule=schedule.every(15).minutes, channel='#choboi-logs')
 def healthcheck(*args, **kwargs):
     # ping api every 15 minutes so heroku free dyno doesn't sleep
-    url = config.API_URL + '/healthcheck'
+    url = config.API_URL + '/health'
     response = requests.get(url)
     if response.status_code == 200:
         return 'healthcheck: ok'
